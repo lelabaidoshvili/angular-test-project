7515750b878984b5d479e3f73897192cd14a967d
lelabaidoshvili <baidoshvililela3@gmail.com>
2023-06-25 14:48:07 +0400

    add routing

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home/home.component';
 import { FormComponent } from './form/form.component';
+import {SharedModule} from "../../shared/shared.module";
 
 
 @NgModule({
   declarations: [
-    HomeComponent,
     FormComponent
   ],
   imports: [
-    CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
