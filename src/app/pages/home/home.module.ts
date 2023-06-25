b36869c3b42321f7d7d9cb103fd764d6f070d72a
lelabaidoshvili <baidoshvililela3@gmail.com>
2023-06-25 14:23:58 +0400

    init

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    FormComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatListModule } from '@angular/material/list';
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatSidenavModule,
+    DragDropModule,
+    MatListModule,
+
+
+  ],
+})
+export class MaterialModule {}
